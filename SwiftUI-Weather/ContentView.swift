b63c9f411f9bd0890796b5cddb9516ac5ab42053
İsmail Can Akgün <ismailcanakg1@gmail.com>
2023-11-27 13:52:27 +0300

    Initial Commit

---
+++ SwiftUI-Weather/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftUI-Weather
+//
+//  Created by İsmail Can Akgün on 27.11.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
